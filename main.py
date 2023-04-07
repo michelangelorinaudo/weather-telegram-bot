ef9510e4891a1447cbd001d9bedab3974f1cdf54
Michelangelo Rinaudo <57008516+michelangelorinaudo@users.noreply.github.com>
2023-04-07 15:59:57 +0200

    feat(main.py): adding functionality to search for weather information by city

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ def welcome(update, context):
 def help(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=f"Please type /weather followed by your city. E.g /weather London")
 
-def get_coordinates(city):
-    url = f"http://api.openweathermap.org/geo/1.0/direct?q={city}&limit=1&appid={openweathermap_api_key}"
+def get_coordinates(city, country_code):
+    url = f"http://api.openweathermap.org/geo/1.0/direct?q={city},{country_code}&limit=1&appid={openweathermap_api_key}"
     response = requests.get(url, params={"appid": openweathermap_api_key})
     data = json.loads(response.text)
     coordinates = {"latitude": data[0]["lat"], "longitude": data[0]["lon"]}
@@ -25,8 +25,10 @@ def get_coordinates(city):
 
 def get_weather(update, context):
     args = context.args
-    city = ' '.join(args)
-    city_coordinates = get_coordinates(city)
+    logging.debug(f"Parameters for request: {args}")
+
+    city_coordinates = get_coordinates(args[0], args[1])
+
     latitude = city_coordinates["latitude"]
     longitude = city_coordinates["longitude"]
     url = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={openweathermap_api_key}"
@@ -35,7 +37,7 @@ def get_weather(update, context):
     logging.info(f"Request response: {data}")
     weather = data["weather"][0]["description"]
     temp = round(data["main"]["temp"] - 273.15, 1)
-    context.bot.send_message(chat_id=update.effective_chat.id, text=f"The weather in {city} is {weather} with a temperature of {temp}°C")
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f"The weather in {args[0]} is {weather} with a temperature of {temp}°C")
 
 def main():
     updater = Updater(telegram_bot_api_key)
